26d328a788ebeb8d893c4327c51f1c61c8bc93e2
EricsonSaputra313 <ericsonsaputra313@gmail.com>
2024-11-11 11:16:52 +0700

    5

--- wisata_candi/lib/screens/detail_screen.dart
+++ wisata_candi/lib/screens/detail_screen.dart
@@ -9,7 +9,20 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: [Stack()],
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                candi.imageAsset,
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
