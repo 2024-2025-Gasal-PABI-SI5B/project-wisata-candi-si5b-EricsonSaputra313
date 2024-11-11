fdb929f22f973fc5b30c68599eed62f089752753
EricsonSaputra313 <ericsonsaputra313@gmail.com>
2024-11-11 10:35:59 +0700

    2

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -10,6 +10,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'Wisata Candi',
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
